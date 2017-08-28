a1b1cb4e0fe5327e7000f5b0242387b99d630190
Akshay Pradeep <akshay.p.pradheep@gmail.com>
2017-08-28 20:38:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+    res.send('the article one requsted and will serve here');
+});
+
+app.get('/article-two', function(req, res){
+    res.send('the article two requsted and will serve here');
+});
+
+app.get('/article-three', function(req, res){
+    res.send('the article three requsted and will serve here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
